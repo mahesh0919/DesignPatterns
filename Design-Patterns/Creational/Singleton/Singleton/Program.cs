07c244c69677e641156edec94d58b83e753b3e90
mahesh <mahesh0919@gmail.com>
2016-07-06 20:20:31 +0530

    Adding Singleton Pattern
    This pattern involves a single class which is responsible to create an
    object while making sure that only single object gets created. This
    class provides a way to access its only object which can be accessed
    directly without need to instantiate the object of the class

---
+++ Design-Patterns/Creational/Singleton/Singleton/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton singleObj = Singleton.GetObject();
+            singleObj.PrintMessage("Hello how are you");
+            Console.Read();
+        }
+    }
+}
---
+++ Design-Patterns/Creational/Singleton/Singleton/Singleton.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class Singleton
+    {
+        protected static Singleton _singleObject;
+
+        // below constructor is to prevent creating new class object
+        private Singleton()
+        {
+
+        }
+
+        public static Singleton GetObject()
+        {
+            if (_singleObject == null)
+            {
+                _singleObject = new Singleton();
+            }
+            return _singleObject;
+        }
+
+        public void PrintMessage(string s)
+        {
+            Console.WriteLine(s);
+        }
+
+    }
+}
