c06e453ab65c9eda8d49f77df6d3d09817f962f4
mahesh <mahesh0919@gmail.com>
2016-07-07 12:22:41 +0530

    Proxy Pattern
    Proxy pattern involves a class, called proxy class, which represents
    functionality of another class.

---
+++ Design-Patterns/Structural/Proxy/Proxy/ProxyWcfApi.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    // This is a proxy class for Real WCF API
+    public class ProxyWcfApi: IWCFApi
+    {
+        private RealWcfApi _realWcfApi;
+
+        public void PerformAction()
+        {
+            if (_realWcfApi == null)
+                _realWcfApi = new RealWcfApi();
+
+            _realWcfApi.PerformAction();
+        }
+    }
+}
---
+++ Design-Patterns/Structural/Proxy/Proxy/RealWcfApi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    public class RealWcfApi : IWCFApi
+    {
+        public void PerformAction()
+        {
+            Console.WriteLine("Real API actions called from Proxy class");
+        }
+    }
+}
